429796bed9849fadef1c083552b994a8694764f9
prosfp <110301233+prosfp@users.noreply.github.com>
2023-12-02 20:53:43 +0100

    Edició completa

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/Layout';
-import NouClient from './pages/NouClient';
+import NouClient, { action as nouClientAction } from './pages/NouClient';
 import Index, { loader as clientsLoader } from './pages/Index';
+import EditarClient, {
+  loader as editarClientLoader,
+} from './pages/EditarClient';
+import ErrorPage from './components/ErrorPage';
 
 const router = createBrowserRouter([
   {
@@ -15,10 +19,18 @@ const router = createBrowserRouter([
         index: true,
         element: <Index />,
         loader: clientsLoader,
+        errorElement: <ErrorPage />,
       },
       {
         path: '/clients/nou',
         element: <NouClient />,
+        action: nouClientAction,
+      },
+      {
+        path: '/clients/:clientID/editar',
+        element: <EditarClient />,
+        loader: editarClientLoader,
+        errorElement: <ErrorPage />,
       },
     ],
   },
